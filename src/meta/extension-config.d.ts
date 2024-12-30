e5e2eb0185b6f84dce1e198a0c8fe11d0bc955b5
Dmitriy Cherchenko <dchenk@users.noreply.github.com>
2024-12-30 12:21:37 -0600

    Add extension config schema

---
+++ src/meta/extension-config.d.ts
@@ -0,0 +1,12 @@
+// DO NOT EDIT. This file was generated. Instead, edit the corresponding JSON Schema file.
+
+export interface ExtensionConfig {
+  name: string;
+  components?: {
+    [k: string]:
+      | {
+          path: string;
+        }
+      | undefined;
+  };
+}
