9b11704a5215c3ef00cf4f334ecffe0a104f3440
Dmitriy <dchenk@users.noreply.github.com>
2025-02-05 12:06:00 -0600

    Add userOrgId to string expression union (#21)

--- src/expressions/array-expression.d.ts
+++ src/expressions/array-expression.d.ts
@@ -51,6 +51,7 @@ export type StringExpression =
   | PropsString
   | StringLiteral
   | UserEmail
+  | UserOrgId
   | UserRole;
 
 export interface ArrayLiteral {
@@ -218,6 +219,9 @@ export interface StringLiteral {
 }
 export interface UserEmail {
   operator: 'userEmail';
+}
+export interface UserOrgId {
+  operator: 'userOrgId';
 }
 export interface UserRole {
   operator: 'userRole';
--- src/expressions/number-expression.d.ts
+++ src/expressions/number-expression.d.ts
@@ -53,6 +53,7 @@ export type StringExpression =
   | PropsString
   | StringLiteral
   | UserEmail
+  | UserOrgId
   | UserRole;
 
 export interface Count {
@@ -196,6 +197,9 @@ export interface StringLiteral {
 }
 export interface UserEmail {
   operator: 'userEmail';
+}
+export interface UserOrgId {
+  operator: 'userOrgId';
 }
 export interface UserRole {
   operator: 'userRole';
